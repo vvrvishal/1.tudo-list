8552598b7257448282fc635d3a6f4c91fbb6f2a5
vvrvishal <68494216+vvrvishal@users.noreply.github.com>
2022-01-17 19:44:25 +0530

    .

--- js/javascript.js
+++ js/javascript.js
@@ -1,41 +1,47 @@
 var counter = 0;
 var enter_task;
 var task_skeleton;
+const task_array = [];
 
-function add_todys_task(){
+function store_value() {
+    var enter_task = document.getElementById('enter_task').value;
+    var task_object = { task: enter_task };
+    task_array.push(task_object);
+    console.log(task_array);
+    add_todys_task(task_object);
+}
+
+function add_todys_task(object) {
     counter++;
     enter_task = document.getElementById('enter_task').value;
     task_skeleton = `<div class="align-items-center border-bottom d-flex justify-content-between" id="task_${counter}">
-                        <p class="mt-2 task"><span class="pr-2"><input type="checkbox" id='checke_${counter}'></span><b>${enter_task}</b></p>
+                        <p class="mt-2 task"><span class="pr-2"><input type="checkbox" id='checke_${counter}'></span><b>${object.task}</b></p>
                         <span>
                             <i class="bg-secondary border fa-trash-alt fas px-3 py-2 ml-4 rounded text-light" id="delete_task_${counter}"></i>
                         </span>
                     </div>`;
-    if(enter_task != ''){
+    if (enter_task != '') {
         document.getElementById('todays_task').innerHTML += task_skeleton;
         document.getElementById('enter_task').value = '';
         console.log(task_skeleton);
-    }else{
+    } else {
         alert('1st enter a task');
     }
-    $(`#task_${counter}`).click(function () {
-        $(this).remove();
+    $(`#delete_task_${counter}`).click(function() {
+        console.log(`task_${counter}`);
     });
     // console.log(task_skeleton);
-    var delete_task = document.getElementById(`delete_task_${counter}`);
-    console.log(delete_task);
+    // var delete_task = document.getElementById(`delete_task_${counter}`);
+    // console.log(delete_task);
 
-delete_task.addEventListener('click',function(){
+    // delete_task.addEventListener('click', function() {
     // document.getElementById(`task_${counter}`).remove();
-    counter--;
-});
-console.log(counter);
-if (`#task_${counter}`!=='') {
-    function task_done(){
-        alert('your task is completed')
+    //     counter--;
+    // });
+    console.log(counter);
+    if (`#task_${counter}` !== '') {
+        function task_done() {
+            alert('your task is completed')
+        }
     }
 }
-}
-
-
-
